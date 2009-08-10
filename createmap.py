4e6fb81671800ee967a180ccaa21586f8b0d1037
Alex Willisson <alex.willisson@gmail.com>
2009-08-10 00:43:02 -0400

    lots of tweaks, the game is sort of playable now

--- createmap.py
+++ createmap.py
@@ -5,7 +5,8 @@ from pygame.locals import *
 
 class Square:
     def __init__ (self, img, coords,
-                  left = None, right = None, up = None, down = None):
+                  left = None, right = None, up = None, down = None,
+                  robot = None, symbol = None):
         self.img = img
         self.coords = coords
         self.left = left
@@ -13,6 +14,20 @@ class Square:
         self.up = up
         self.down = down
         self.box = pygame.Rect (coords[0] * 40, coords[1] * 40, 40, 40)
+        self.robot = robot
+        self.symbol = symbol
+
+class Robot:
+    def __init__ (self, img, color, square = None):
+        self.img = img
+        self.color = color
+        self.square = square
+
+class Symbol:
+    def __init__ (self, img, color, square = None):
+        self.img = img
+        self.color = color
+        self.square = square
 
 def draw ():
     screen.fill ((0xff, 0xff, 0xff))
@@ -21,6 +36,27 @@ def draw ():
         for square in row:
             screen.blit (square.img, (square.coords[0] * 40,
                                       square.coords[1] * 40))
+
+
+    if red.square:
+        screen.blit (red.img, (red.square.coords[0] * 40,
+                               red.square.coords[1] * 40))
+    if blue.square:
+        screen.blit (blue.img, (blue.square.coords[0] * 40,
+                                blue.square.coords[1] * 40))
+    if green.square:
+        screen.blit (green.img, (green.square.coords[0] * 40,
+                                 green.square.coords[1] * 40))
+    if yellow.square:
+        screen.blit (yellow.img, (yellow.square.coords[0] * 40,
+                                  yellow.square.coords[1] * 40))
+
+    for color in symbols:
+        for form in symbols[color]:
+            if symbols[color][form].square:
+                screen.blit (symbols[color][form].img,
+                             (symbols[color][form].square.coords[0] * 40,
+                              symbols[color][form].square.coords[1] * 40))
 
     for row in grid:
         for square in row:
@@ -74,14 +110,18 @@ def gengrid ():
             if grid[x][y].coords[1] != 15:
                 grid[x][y].down = grid[x][y+1]
 
-    grid = mkwalls (grid)
+    grid = loadmap (grid)
 
     return grid
 
-def mkwalls (grid):
-
-    #File line format: (x, y) left right up down
-    #   Example: (14, 10) 1 0 1 0
+def loadmap (grid):
+    #File line format: (x, y)
+    # left right up down redbot bluebot greenbot yellowbot
+    #  redbio redhex redtar redtri
+    #  bluebio bluehex bluetar bluetri
+    #  greenbio greenhex greentar greentri
+    #  yellowbio yellowhex yellowtar yellowtri
+    #   Example: (14, 10) 1 0 1 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
 
     file = open ("first.map")
     line = file.readline ()
@@ -112,7 +152,67 @@ def mkwalls (grid):
             if cursquare.down:
                 cursquare.down.up = None
                 cursquare.down = None
-            
+        if data[6]:
+            red.square = cursquare
+            red.square.robot = red
+        if data[7]:
+            blue.square = cursquare
+            blue.square.robot = blue
+        if data[8]:
+            green.square = cursquare
+            green.square.robot = green
+        if data[9]:
+            yellow.square = cursquare
+            yellow.square.robot = yellow
+        if data[10]:
+            symbols['red']['bio'].square = cursquare
+            cursquare.symbol = symbols['red']['bio']
+        if data[11]:
+            symbols['red']['hex'].square = cursquare
+            cursquare.symbol = symbols['red']['hex']
+        if data[12]:
+            symbols['red']['tar'].square = cursquare
+            cursquare.symbol = symbols['red']['tar']
+        if data[13]:
+            symbols['red']['tri'].square = cursquare
+            cursquare.symbol = symbols['red']['tri']
+        if data[14]:
+            symbols['blue']['bio'].square = cursquare
+            cursquare.symbol = symbols['blue']['bio']
+        if data[15]:
+            symbols['blue']['hex'].square = cursquare
+            cursquare.symbol = symbols['blue']['hex']
+        if data[16]:
+            symbols['blue']['tar'].square = cursquare
+            cursquare.symbol = symbols['blue']['tar']
+        if data[17]:
+            symbols['blue']['tri'].square = cursquare
+            cursquare.symbol = symbols['blue']['tri']
+        if data[18]:
+            symbols['green']['bio'].square = cursquare
+            cursquare.symbol = symbols['green']['bio']
+        if data[19]:
+            symbols['green']['hex'].square = cursquare
+            cursquare.symbol = symbols['green']['hex']
+        if data[20]:
+            symbols['green']['tar'].square = cursquare
+            cursquare.symbol = symbols['green']['tar']
+        if data[21]:
+            symbols['green']['tri'].square = cursquare
+            cursquare.symbol = symbols['green']['tri']
+        if data[22]:
+            symbols['yellow']['bio'].square = cursquare
+            cursquare.symbol = symbols['yellow']['bio']
+        if data[23]:
+            symbols['yellow']['hex'].square = cursquare
+            cursquare.symbol = symbols['yellow']['hex']
+        if data[24]:
+            symbols['yellow']['tar'].square = cursquare
+            cursquare.symbol = symbols['yellow']['tar']
+        if data[25]:
+            symbols['yellow']['tri'].square = cursquare
+            cursquare.symbol = symbols['yellow']['tri']
+
         line = file.readline ()
 
     return grid
@@ -140,6 +240,30 @@ def savemap ():
                 string += " 0"
             else:
                 string += " 1"
+            if square.robot and square.robot.color == "red":
+                string += " 1"
+            else:
+                string += " 0"
+            if square.robot and square.robot.color == "blue":
+                string += " 1"
+            else:
+                string += " 0"
+            if square.robot and square.robot.color == "green":
+                string += " 1"
+            else:
+                string += " 0"
+            if square.robot and square.robot.color == "yellow":
+                string += " 1"
+            else:
+                string += " 0"
+
+            for color in symbols:
+                for form in symbols[color]:
+                    if symbols[color][form].square == square:
+                        string += " 1"
+                    else:
+                        string += " 0"
+
             string += "\n"
 
     file.write (string)
@@ -151,6 +275,23 @@ screen = pygame.display.set_mode ((640, 640))
 pygame.display.set_caption ('Ricochet Robots')
 
 squareimg = pygame.image.load ("./square.png")
+
+red = Robot (pygame.image.load ("./redbot.png"), "red")
+blue = Robot (pygame.image.load ("./bluebot.png"), "blue")
+green = Robot (pygame.image.load ("./greenbot.png"), "green")
+yellow = Robot (pygame.image.load ("./yellowbot.png"), "yellow")
+
+symbols = {'red': {}, 'blue': {}, 'green': {}, 'yellow': {}}
+
+for color in symbols:
+    symbols[color]['bio'] = Symbol (pygame.image.load
+                                    ("./" + color + "bio.png"), color)
+    symbols[color]['hex'] = Symbol (pygame.image.load
+                                    ("./" + color + "hex.png"), color)
+    symbols[color]['tar'] = Symbol (pygame.image.load
+                                    ("./" + color + "tar.png"), color)
+    symbols[color]['tri'] = Symbol (pygame.image.load
+                                    ("./" + color + "tri.png"), color)
 
 grid = gengrid ()
 
@@ -198,6 +339,10 @@ while True:
                     if down:
                         selected.down = down
                         selected.down.up = selected
+                elif state == 1:
+                    if selected and selected.robot:
+                        selected.robot.square = None
+                        selected.robot = None
             elif event.key == K_LEFT:
                 if selected and selected.left:
                     selected = selected.left
@@ -211,28 +356,137 @@ while True:
                 if selected and selected.down:
                     selected = selected.down
             elif event.unicode == 's':
-                if state == 1:
+                state += 1
+                if state == 6:
                     savemap ()
                     sys.exit ()
-                state += 1
-            elif event.unicode == 'u':
+                print "newstate: ", state
+            elif event.unicode == 'k':
                 if state == 0 and selected and selected.up:
                     selected.up.down = None
                     selected.up = None
-            elif event.unicode == 'd':
+            elif event.unicode == 'j':
                 if state == 0 and selected and selected.down:
                     selected.down.up = None
                     selected.down = None
-            elif event.unicode == 'l':
+            elif event.unicode == 'h':
                 if state == 0 and selected and selected.left:
                     selected.left.right = None
                     selected.left = None
-            elif event.unicode == 'r':
+
+                symstate = 0
+                if state == 2:
+                    color = 'red'
+                    symstate = 1
+                elif state == 3:
+                    color = 'blue'
+                    symstate = 1
+                elif state == 4:
+                    color = 'green'
+                    symstate = 1
+                elif state == 5:
+                    color = 'yellow'
+                    symstate = 1
+
+                if symstate and selected:
+                    if symbols[color]['hex'].square:
+                        symbols[color]['hex'].square.symbol = None
+                    if not selected.symbol:
+                        selected.symbol = symbols[color]['hex']
+                        symbols[color]['hex'].square = selected
+            elif event.unicode == 'l':
                 if state == 0 and selected and selected.right:
                     selected.right.left = None
                     selected.right = None
-                elif state == 1:
+            elif event.unicode == 'r':
+                if state == 1 and selected:
+                    if red.square:
+                        red.square.robot = None
+                    if not selected.robot:
+                        selected.robot = red
+                        red.square = selected
+            elif event.unicode == 'g':
+                if state == 1 and selected:
+                    if green.square:
+                        green.square.robot = None
+                    if not selected.robot:
+                        selected.robot = green
+                        green.square = selected
+                symstate = 0
+                if state == 2:
+                    color = 'red'
+                    symstate = 1
+                elif state == 3:
+                    color = 'blue'
+                    symstate = 1
+                elif state == 4:
+                    color = 'green'
+                    symstate = 1
+                elif state == 5:
+                    color = 'yellow'
+                    symstate = 1
+
+                if symstate and selected:
+                    if symbols[color]['tar'].square:
+                        symbols[color]['tar'].square.symbol = None
+                    if not selected.symbol:
+                        selected.symbol = symbols[color]['tar']
+                        symbols[color]['tar'].square = selected
+            elif event.unicode == 'b':
+                if state == 1 and selected:
+                    if blue.square:
+                        blue.square.robot = None
+                    if not selected.robot:
+                        selected.robot = blue
+                        blue.square = selected
+                symstate = 0
+                if state == 2:
+                    color = 'red'
+                    symstate = 1
+                elif state == 3:
+                    color = 'blue'
+                    symstate = 1
+                elif state == 4:
+                    color = 'green'
+                    symstate = 1
+                elif state == 5:
+                    color = 'yellow'
+                    symstate = 1
                     
+                if symstate and selected:
+                    if symbols[color]['bio'].square:
+                        symbols[color]['bio'].square.symbol = None
+                    if not selected.symbol:
+                        selected.symbol = symbols[color]['bio']
+                        symbols[color]['bio'].square = selected
+            elif event.unicode == 'y':
+                if state == 1 and selected:
+                    if yellow.square:
+                        yellow.square.robot = None
+                    if not selected.robot:
+                        selected.robot = yellow
+                        yellow.square = selected
+            elif event.unicode == 't':
+                symstate = 0
+                if state == 2:
+                    color = 'red'
+                    symstate = 1
+                elif state == 3:
+                    color = 'blue'
+                    symstate = 1
+                elif state == 4:
+                    color = 'green'
+                    symstate = 1
+                elif state == 5:
+                    color = 'yellow'
+                    symstate = 1
+
+                if symstate and selected:
+                    if symbols[color]['tri'].square:
+                        symbols[color]['tri'].square.symbol = None
+                    if not selected.symbol:
+                        selected.symbol = symbols[color]['tri']
+                        symbols[color]['tri'].square = selected
         elif event.type == MOUSEMOTION:
             mousepos = event.pos
         elif event.type == MOUSEBUTTONDOWN:
--- ricochet.py
+++ ricochet.py
@@ -5,7 +5,8 @@ from pygame.locals import *
 
 class Square:
     def __init__ (self, img, coords,
-                  left = None, right = None, up = None, down = None):
+                  left = None, right = None, up = None, down = None,
+                  robot = None, symbol = None):
         self.img = img
         self.coords = coords
         self.left = left
@@ -13,6 +14,20 @@ class Square:
         self.up = up
         self.down = down
         self.box = pygame.Rect (coords[0] * 40, coords[1] * 40, 40, 40)
+        self.robot = robot
+        self.symbol = symbol
+
+class Robot:
+    def __init__ (self, img, color, square = None):
+        self.img = img
+        self.color = color
+        self.square = square
+
+class Symbol:
+    def __init__ (self, img, color, square = None):
+        self.img = img
+        self.color = color
+        self.square = square
 
 def draw ():
     screen.fill ((0xff, 0xff, 0xff))
@@ -22,6 +37,26 @@ def draw ():
             screen.blit (square.img, (square.coords[0] * 40,
                                       square.coords[1] * 40))
 
+    if red.square:
+        screen.blit (red.img, (red.square.coords[0] * 40,
+                               red.square.coords[1] * 40))
+    if blue.square:
+        screen.blit (blue.img, (blue.square.coords[0] * 40,
+                                blue.square.coords[1] * 40))
+    if green.square:
+        screen.blit (green.img, (green.square.coords[0] * 40,
+                                 green.square.coords[1] * 40))
+    if yellow.square:
+        screen.blit (yellow.img, (yellow.square.coords[0] * 40,
+                                  yellow.square.coords[1] * 40))
+
+    for color in symbols:
+        for form in symbols[color]:
+            if symbols[color][form].square:
+                screen.blit (symbols[color][form].img,
+                             (symbols[color][form].square.coords[0] * 40,
+                              symbols[color][form].square.coords[1] * 40))
+
     for row in grid:
         for square in row:
             if not square.right:
@@ -30,40 +65,31 @@ def draw ():
                                    square.coords[1] * 40),
                                   (square.coords[0] * 40 + 40 - 1,
                                    square.coords[1] * 40 + 40),
-                                  2)
+                                  5)
             if not square.left:
                 pygame.draw.line (screen, (0xff, 0x0, 0x0),
                                   (square.coords[0] * 40,
                                    square.coords[1] * 40),
                                   (square.coords[0] * 40,
                                    square.coords[1] * 40 + 40),
-                                  2)
+                                  5)
             if not square.up:
                 pygame.draw.line (screen, (0xff, 0x0, 0x0),
                                   (square.coords[0] * 40,
                                    square.coords[1] * 40),
                                   (square.coords[0] * 40 + 40 - 1,
                                    square.coords[1] * 40),
-                                  2)
+                                  5)
             if not square.down:
                 pygame.draw.line (screen, (0xff, 0x0, 0x0),
                                   (square.coords[0] * 40,
                                    square.coords[1] * 40 + 40),
                                   (square.coords[0] * 40 + 40,
                                    square.coords[1] * 40 + 40),
-                                  2)
+                                  5)
 
     if selected:
-        pygame.draw.rect (screen, (0xff, 0xff, 0x0), selected.box, 3)
-
-        if selected.left:
-            pygame.draw.rect (screen, (0x0, 0x0, 0xff), selected.left.box, 2)
-        if selected.right:
-            pygame.draw.rect (screen, (0x0, 0x0, 0xff), selected.right.box, 2)
-        if selected.up:
-            pygame.draw.rect (screen, (0x0, 0x0, 0xff), selected.up.box, 2)
-        if selected.down:
-            pygame.draw.rect (screen, (0x0, 0x0, 0xff), selected.down.box, 2)
+        pygame.draw.rect (screen, (0xff, 0xff, 0x0), selected.box, 1)
 
 def gengrid ():
     grid = []
@@ -83,14 +109,18 @@ def gengrid ():
             if grid[x][y].coords[1] != 15:
                 grid[x][y].down = grid[x][y+1]
 
-    grid = mkwalls (grid)
+    grid = loadmap (grid)
 
     return grid
 
-def mkwalls (grid):
-
-    #File line format: (x, y) left right up down
-    #   Example: (14, 10) 1 0 1 0
+def loadmap (grid):
+    #File line format: (x, y)
+    # left right up down redbot bluebot greenbot yellowbot
+    #  redbio redhex redtar redtri
+    #  bluebio bluehex bluetar bluetri
+    #  greenbio greenhex greentar greentri
+    #  yellowbio yellowhex yellowtar yellowtri
+    #   Example: (14, 10) 1 0 1 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
 
     file = open ("first.map")
     line = file.readline ()
@@ -102,8 +132,6 @@ def mkwalls (grid):
                 data.append (int (num))
 
         coords = data[:2]
-        print data
-        print coords
 
         cursquare = grid[coords[0]][coords[1]]
 
@@ -123,7 +151,67 @@ def mkwalls (grid):
             if cursquare.down:
                 cursquare.down.up = None
                 cursquare.down = None
-            
+        if data[6]:
+            red.square = cursquare
+            red.square.robot = red
+        if data[7]:
+            blue.square = cursquare
+            blue.square.robot = blue
+        if data[8]:
+            green.square = cursquare
+            green.square.robot = green
+        if data[9]:
+            yellow.square = cursquare
+            yellow.square.robot = yellow
+        if data[10]:
+            symbols['red']['bio'].square = cursquare
+            cursquare.symbol = symbols['red']['bio']
+        if data[11]:
+            symbols['red']['hex'].square = cursquare
+            cursquare.symbol = symbols['red']['hex']
+        if data[12]:
+            symbols['red']['tar'].square = cursquare
+            cursquare.symbol = symbols['red']['tar']
+        if data[13]:
+            symbols['red']['tri'].square = cursquare
+            cursquare.symbol = symbols['red']['tri']
+        if data[14]:
+            symbols['blue']['bio'].square = cursquare
+            cursquare.symbol = symbols['blue']['bio']
+        if data[15]:
+            symbols['blue']['hex'].square = cursquare
+            cursquare.symbol = symbols['blue']['hex']
+        if data[16]:
+            symbols['blue']['tar'].square = cursquare
+            cursquare.symbol = symbols['blue']['tar']
+        if data[17]:
+            symbols['blue']['tri'].square = cursquare
+            cursquare.symbol = symbols['blue']['tri']
+        if data[18]:
+            symbols['green']['bio'].square = cursquare
+            cursquare.symbol = symbols['green']['bio']
+        if data[19]:
+            symbols['green']['hex'].square = cursquare
+            cursquare.symbol = symbols['green']['hex']
+        if data[20]:
+            symbols['green']['tar'].square = cursquare
+            cursquare.symbol = symbols['green']['tar']
+        if data[21]:
+            symbols['green']['tri'].square = cursquare
+            cursquare.symbol = symbols['green']['tri']
+        if data[22]:
+            symbols['yellow']['bio'].square = cursquare
+            cursquare.symbol = symbols['yellow']['bio']
+        if data[23]:
+            symbols['yellow']['hex'].square = cursquare
+            cursquare.symbol = symbols['yellow']['hex']
+        if data[24]:
+            symbols['yellow']['tar'].square = cursquare
+            cursquare.symbol = symbols['yellow']['tar']
+        if data[25]:
+            symbols['yellow']['tri'].square = cursquare
+            cursquare.symbol = symbols['yellow']['tri']
+
         line = file.readline ()
 
     return grid
@@ -134,6 +222,23 @@ screen = pygame.display.set_mode ((640, 640))
 pygame.display.set_caption ('Ricochet Robots')
 
 squareimg = pygame.image.load ("./square.png")
+
+red = Robot (pygame.image.load ("./redbot.png"), "red")
+blue = Robot (pygame.image.load ("./bluebot.png"), "blue")
+green = Robot (pygame.image.load ("./greenbot.png"), "green")
+yellow = Robot (pygame.image.load ("./yellowbot.png"), "yellow")
+
+symbols = {'red': {}, 'blue': {}, 'green': {}, 'yellow': {}}
+
+for color in symbols:
+    symbols[color]['bio'] = Symbol (pygame.image.load
+                                    ("./" + color + "bio.png"), color)
+    symbols[color]['hex'] = Symbol (pygame.image.load
+                                    ("./" + color + "hex.png"), color)
+    symbols[color]['tar'] = Symbol (pygame.image.load
+                                    ("./" + color + "tar.png"), color)
+    symbols[color]['tri'] = Symbol (pygame.image.load
+                                    ("./" + color + "tri.png"), color)
 
 grid = gengrid ()
 
@@ -148,13 +253,51 @@ while True:
         elif event.type == KEYDOWN:
             if event.key == K_ESCAPE:
                 sys.exit ()
+            elif event.key == K_LEFT:
+                if selected:
+                    while selected.left and not selected.left.robot:
+                        selected.left.robot = selected.robot
+                        selected.robot.square = selected.left
+                        selected.robot = None
+                        selected = selected.left
+            elif event.key == K_RIGHT:
+                if selected:
+                    while selected.right and not selected.right.robot:
+                        selected.right.robot = selected.robot
+                        selected.robot.square = selected.right
+                        selected.robot = None
+                        selected = selected.right
+            elif event.key == K_UP:
+                if selected:
+                    while selected.up and not selected.up.robot:
+                        selected.up.robot = selected.robot
+                        selected.robot.square = selected.up
+                        selected.robot = None
+                        selected = selected.up
+            elif event.key == K_DOWN:
+                if selected:
+                    while selected.down and not selected.down.robot:
+                        selected.down.robot = selected.robot
+                        selected.robot.square = selected.down
+                        selected.robot = None
+                        selected = selected.down
+            elif event.unicode == 'r':
+                selected = red.square
+            elif event.unicode == 'b':
+                selected = blue.square
+            elif event.unicode == 'g':
+                selected = green.square
+            elif event.unicode == 'y':
+                selected = yellow.square
         elif event.type == MOUSEMOTION:
             mousepos = event.pos
         elif event.type == MOUSEBUTTONDOWN:
             for row in grid:
                 for square in row:
-                    if square.box.collidepoint (mousepos):
+                    if square.box.collidepoint (mousepos) and square.robot:
                         selected = square
 
     draw ()
     pygame.display.flip ()
+
+    pygame.time.wait (100)
